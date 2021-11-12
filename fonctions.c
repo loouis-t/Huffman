69e238ba7e5c27971e3e8c757c5d12f552cfa0a0
Louis Travaux <70093050+loouis-t@users.noreply.github.com>
2021-11-12 12:55:21 +0100

    Update fonctions.c

--- fonctions.c
+++ fonctions.c
@@ -9,20 +9,19 @@ int estFeuille(arbre a) {
     return (estVide(a->gauche) && estVide(a->droit));
 }
 
-int getCaractere(arbre a) {
-    return a->caractere;
+int getCaractere(maillon m) {
+    return m->caractere;
 }
 
-int getOccurrence(arbre a) {
-    return a->occurrence;
+int getOccurrence(maillon m) {
+    return m->occurrence;
 }
 
-arbre creerArbre(int c, int occ, arbre fg, arbre fd) {
+arbre creerArbre(maillon m, arbre fg, arbre fd) {
     arbre ab = (arbre) malloc(sizeof(arbre));
     ab->gauche = fg;
     ab->droit = fd;
-    ab->caractere = c;
-ab->occurrence = occ;
+    ab->maillon = m;
 
     return ab;
 }
@@ -35,21 +34,46 @@ arbre getFilsDroit(arbre a) {
     return a->droit;
 }
 
-arbre countOccurrences(FILE* fichier_source) {
+maillon creerMaillon(int c, int occ, maillon element) {
+    maillon m = (maillon) malloc(sizeof(maillon));
+    m->caractere = c;
+    m->occurrence = occ;
+    m->suivant = element;
+
+    return m;
+}
+
+maillon getMaillon(maillon m) {
+    return m->suivant;
+}
+
+void afficherListeMaillons(maillon m) {
+    printf("%d", getCaractere(m));
+    printf("%d\n", getOccurrence(m));
+    if (m->suivant != NULL) {
+        afficherListeMaillons(m->suivant);
+    }
+}
+
+maillon countOccurrences(FILE* fichier_source) {
     int caractere = 0;
+    int first_c;
     int first_c_occ = 1; //init à 1 pour ++ par la suite
-    arbre ab = NULL;
+    maillon m = NULL;
 
     if (fichier_source != NULL) {
-        //lire caractere par caractere
-        int first_c = (int)fgetc(fichier_source); //stocker premier caractere (ascii) --> comparer par la suite
-        do {
-            caractere = fgetc(fichier_source); //récupérer caractere un par un dans texte
-            if ((int)caractere == first_c) {
-                first_c_occ++; //compter occurrences caractere
-            }
-        } while(caractere != EOF); //EOF: End Of File
-        ab = creerArbre(first_c, first_c_occ, NULL, NULL);
+
+        first_c = (int)fgetc(fichier_source); //stocker premier caractere (ascii) --> comparer par la suite
+        if (first_c == EOF) {
+            return m;
+        } else {
+            do {
+                caractere = fgetc(fichier_source); //récupérer caractere un par un dans texte
+                if ((int)caractere == first_c) {
+                    first_c_occ++; //compter occurrences caractere
+                }
+            } while(caractere != EOF); //EOF: End Of File
+            return creerMaillon(first_c, first_c_occ, countOccurrences(*fichier_source[1]));
+        }
     }
-    return ab;
 }

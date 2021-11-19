0b3777f9a0324ec927901825dc6e5d960f784595
Louis Travaux <70093050+loouis-t@users.noreply.github.com>
2021-11-19 11:16:59 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 //programme principal
 #include "fonctions.h"
 
-
 int main(int argc, char *argv[]){
     maillon m = countOccurrences(convertirFileChar("lorem.txt"), malloc(128), 0);   // compter les occurrences de chaque caractere
-    afficherListeMaillons(m);   // afficher la liste des maillons
+    printf("OK 0\n");
+    maillon m_sorted = convertirTabMaillon(trierListeMaillons(m), 0);
+    afficherListeMaillons(m_sorted);
 }
 
 // COMPILATION:
